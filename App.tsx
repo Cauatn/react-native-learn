06eb272aa31f7b421d6553b61d3ff704f59fab4f
cauatn <tavarescauac@gmail.com>
2024-07-23 10:29:22 -0300

    chore: Add styled-components and theme provider to App component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
 import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components/native";
+
+import {} from "expo-font";
+
+import theme from "./src/theme";
+import { StatusBar, Text } from "react-native";
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
+      <Groups />
+    </ThemeProvider>
+  );
 }
