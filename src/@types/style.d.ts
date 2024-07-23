52b4906c075eccb0a478bd0f46deb0865f16a4fb
cauatn <tavarescauac@gmail.com>
2024-07-23 10:29:10 -0300

    feat: Add styled-components types and Loading component
    The code changes include adding styled-components types to the project and implementing a Loading component with its associated styles.

---
+++ src/@types/style.d.ts
@@ -0,0 +1,7 @@
+import "styled-components";
+import theme from "./src/theme";
+
+declare module "styled-components" {
+  type Theme = typeof theme;
+  export interface DefaultTheme extends ThemeType {}
+}
